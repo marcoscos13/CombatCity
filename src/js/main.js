24aa0250f4730e9bbe8b72c563e5b440083890a7
Unknown <kelvinco@ucm.es>
2017-11-28 22:31:18 +0100

    Images from url not working, loading from file again

--- src/js/main.js
+++ src/js/main.js
@@ -23,8 +23,8 @@ var PreloaderScene = {
     this.load.setPreloadSprite(this.loadingBar);
 
     // TODO: load here the assets for the game
-    this.game.load.crossOrigin = '';
-    this.game.load.baseURL = 'https://marcoscos13.github.io/CombatCity/src/';
+    //this.game.load.crossOrigin = '';
+    //this.game.load.baseURL = 'https://marcoscos13.github.io/CombatCity/src/';
     this.game.load.image('logo', 'images/phaser.png');
     this.game.load.image('tank', 'images/tanque.png');
     this.game.load.image('bullet', 'images/muro.png');
