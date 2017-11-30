febad8e76d0a72265e00cf8698d8c3063e9fb7ae
Marcos García García <marcos13@ucm.es>
2017-11-30 13:26:25 +0100

    Removed inheritance to test game on server

--- src/js/play_scene.js
+++ src/js/play_scene.js
@@ -192,3 +192,151 @@ function collisionHandler (bullet, block) {
     bullet.kill();
     block.kill();
 }
+
+
+
+////////////////
+/////CLASES/////
+////////////////
+
+////Clase Collider y sus métodos
+var Collider = function (game, pos, scale, sprite) {
+    Phaser.Sprite.call(this, game, pos._x, pos._y, sprite);
+    game.physics.enable(this, Phaser.Physics.ARCADE); //Activa las fisicas arcade para este objeto
+    this.enableBody = true;
+    this.physicsBodyType = Phaser.Physics.ARCADE;
+    this.smoothed = false;
+    this._scale = scale;
+    this.scale.setTo(scale._x, scale._y);
+    this.anchor.setTo(0.5, 0.5);
+    game.add.existing(this);
+};
+
+Collider.prototype = Object.create(Phaser.Sprite.prototype);
+Collider.prototype.constructor = Collider;
+
+////Clase Movable y sus métodos
+var Movable = function (game, pos, scale, vel, dir, sprite){
+    Collider.apply(this, [game, pos, scale, sprite]);
+    this._velocity = vel;
+    this._direction = dir;
+}
+
+Movable.prototype = Object.create(Collider.prototype);
+Movable.prototype.constructor = Movable;
+Movable.prototype.set_velocity = function()
+{ //Función para cambiar la velocidad
+    this._velocity._x = velocity._x;
+    this._velocity._y = velocity._y;
+}
+Movable.prototype.update = function()
+{ //Update de los móviles
+    // this.x += this._direction._x * this._velocity._x;
+    // this.y += this._direction._y * this._velocity._y;
+}
+
+////Clase Bullet y sus métodoss
+var Bullet = function (game, pos, scale, vel, dir, sprite){
+    Movable.apply(this, [game, pos, scale, vel, dir, sprite]);
+}
+
+Bullet.prototype = Object.create(Movable.prototype);
+Bullet.prototype.constructor = Bullet;
+
+////Clase Shooter y sus métodos
+var Shooter = function(game, pos, scale, vel, dir, bulletsGroup, bulletVel, bulletTime, sprite){
+    Movable.apply(this, [game, pos, scale, vel, dir, sprite]);
+    this._bulletsGroup = bulletsGroup;
+    this._bulletVel = bulletVel;
+    this._bulletTime = bulletTime;
+    this._bulletSince = 0;
+    this._game = game;
+}
+
+Shooter.prototype = Object.create(Movable.prototype);
+Shooter.prototype.constructor = Shooter;
+Shooter.prototype.fire_bullet = function()
+{ //Función para disparar
+    if (this._game.time.now > this._bulletSince)
+    {
+        var bullet = this._bulletsGroup.getFirstExists(false);
+
+        if (bullet)
+        {
+            bullet.reset(this.x, this.y);
+            bullet.body.velocity.y = this._bulletVel * this._direction._y;
+            bullet.body.velocity.x = this._bulletVel * this._direction._x;
+            this._bulletSince = this._game.time.now + this._bulletTime;
+        }
+    }
+}
+
+////Clase Player y sus métodos
+var Player = function(game, pos, scale, vel, dir, bulletsGroup, bulletVel, bulletTime, cursors, sprite){
+    Shooter.apply(this, [game, pos, scale, vel, dir, bulletsGroup, bulletVel, bulletTime, sprite]);
+    //Movable.apply(this, [game, pos, scale, vel, dir, sprite]);
+    this._cursors = cursors;
+    this._direction._x = 0;
+    this._direction._y = -1;
+    this.angle = 0;
+}
+
+//Player.prototype = Object.create(Shooter.prototype);
+Player.prototype = Object.create(Shooter.prototype);
+Player.prototype.constructor = Player;
+
+Player.prototype.update = function(){
+    
+    if (this._cursors.left.isDown)
+    {
+        if (this._direction._x !== 0){
+            this.y = 24 * Math.round(this.y/24);
+        }
+        this.body.velocity.y = 0;
+        this.body.velocity.x = -this._velocity._x;
+        this._direction._x = -1;
+        this._direction._y = 0;
+        this.angle = 180;
+    }
+    else if (this._cursors.right.isDown)
+    {
+        if (this._direction._x !== 0){
+            this.y = 24 * Math.round(this.y/24);
+        }
+        this.body.velocity.y = 0;
+        this.body.velocity.x = this._velocity._x;
+        this._direction._x = 1;
+        this._direction._y = 0;
+        this.angle = 0;
+    }
+    else if (this._cursors.down.isDown)
+    {
+        if (this._direction._y !== 0){
+            this.x = 24 * Math.round(this.x/24);
+        }
+        this.body.velocity.x = 0;
+        this.body.velocity.y = this._velocity._y;
+        this._direction._x = 0;
+        this._direction._y = 1;
+        this.angle = 90;
+    }
+    else if (this._cursors.up.isDown)
+    {
+        if (this._direction._y !== 0){
+            this.x = 24 * Math.round(this.x/24);
+        }
+        this.body.velocity.x = 0;
+        this.body.velocity.y = -this._velocity._y;
+        this._direction._x = 0;
+        this._direction._y = -1;
+        this.angle = 270;
+    }else{
+        this.body.velocity.x = 0;
+        this.body.velocity.y = 0;
+    }
+
+    if (this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR))
+    {
+        this.fire_bullet();
+    }
+}
